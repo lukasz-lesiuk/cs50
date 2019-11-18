9cec377ff4326cbdf77013b803cdc6f9006bccfe
lukasz-lesiuk <51463482+lukasz-lesiuk@users.noreply.github.com>
2019-11-18 21:11:20 +0100

    Update crack.py
    Final version - little bit faster, and way more compact

--- crack.py
+++ crack.py
@@ -2,86 +2,37 @@ import sys
 from sys import argv
 import string
 import crypt
+from itertools import product
 
-#source: https://stackoverflow.com/questions/35328953/how-to-compare-individual-characters-in-two-strings-in-python-3
-#def compare(key, password):
-#    for x, y in zip(key, password):
-#        if x == y:
-#            print("PASSWORD FOUND")
-#            print("key = ", key)
-#            print("password = ", password)
-#            sys.exit(0)
 
 def main(argv):
 
-    #check if correct number of arguments
+    # check if correct number of arguments
     if len(argv) != 2:
         exit("Usage: python crack.py infile outfile error")
-    #put password into variable
+    # put password into variable
     password = argv[1]
+    pass_len = 5
 
-    #get list of all relevant letters
-    letters_lower = string.ascii_lowercase
-    letters_upper = string.ascii_uppercase
-    letters = letters_lower + letters_upper
-    print(letters)
-    #get a salt
+    # get list of all relevant letters
+    letters = string.ascii_letters
+    # get letterst as a list
+    letters = list(letters)
+    # print(letters)
+    # get a salt
     salt = password[0] + password[1]
 
-    #check all 1 letter passwords
-    for key_letter1 in letters:
-        key = key_letter1
-        print(key)
-        key_hashed = crypt.crypt(key, salt)
-        if password == key_hashed:
-            print("PASSWORD FOUND:", key)
-            sys.exit(0)
-
-    #check all 2 letter passwords
-    for key_letter1 in letters:
-        for key_letter2 in letters:
-            key = key_letter1 + key_letter2
-            print(key)
-            key_hashed = crypt.crypt(key, salt)
+    # Get possible password check if it is correct and discard
+    for i in range(pass_len):#
+        for guess_list in list(product(letters, repeat=i)):
+            guess = ""
+            for j in range(i):
+                guess = guess + guess_list[j]
+            # print(guess)
+            key_hashed = crypt.crypt(guess, salt)
             if password == key_hashed:
-                print("PASSWORD FOUND:", key)
+                print("PASSWORD FOUND:", guess)
                 sys.exit(0)
-
-    #check all 3 letter passwords
-    for key_letter1 in letters:
-        for key_letter2 in letters:
-            for key_letter3 in letters:
-                key = key_letter1 + key_letter2 + key_letter3
-                print(key)
-                key_hashed = crypt.crypt(key, salt)
-                if password == key_hashed:
-                    print("PASSWORD FOUND:", key)
-                    sys.exit(0)
-
-    #check all 4 letter passwords
-    for key_letter1 in letters:
-        for key_letter2 in letters:
-            for key_letter3 in letters:
-                for key_letter4 in letters:
-                    key = key_letter1 + key_letter2 + key_letter3 + key_letter4
-                    print(key)
-                    key_hashed = crypt.crypt(key, salt)
-                    if password == key_hashed:
-                        print("PASSWORD FOUND:", key)
-                        sys.exit(0)
-
-    #check all 5 letter passwords
-        for key_letter1 in letters:
-            for key_letter2 in letters:
-                for key_letter3 in letters:
-                    for key_letter4 in letters:
-                        for key_letter5 in letters:
-                            key = key_letter1 + key_letter2 + key_letter3 + key_letter4 + key_letter5
-                            print(key)
-                            key_hashed = crypt.crypt(key, salt)
-                            if password == key_hashed:
-                                print("PASSWORD FOUND:", key)
-                                sys.exit(0)
 
 
 if __name__ == "__main__":
